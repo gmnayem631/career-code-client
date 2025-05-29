27768bd4e4ed6f996d0b24088c56b5bdd665fdb4
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-05-29 21:06:20 +0600

    added job cards

--- src/pages/Home/HotJobs.jsx
+++ src/pages/Home/HotJobs.jsx
@@ -1,9 +1,19 @@
 import React, { use } from "react";
+import JobCard from "../shared/JobCard";
 
 const HotJobs = ({ jobsPromise }) => {
   const jobs = use(jobsPromise);
   console.log(jobs);
-  return <div>Total Jobs: {jobs.length}</div>;
+  return (
+    <div>
+      <h2 className="text-4xl my-6 font-semibold">Hot Jobs of the Day!</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3">
+        {jobs.map((job) => (
+          <JobCard key={job._id} job={job}></JobCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HotJobs;
