b56c9a08ff5f5bc7f869722c2794026e28b7e56c
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-06-02 00:00:21 +0600

    added basic form for adding jobs

--- src/pages/AddJobs/AddJob.jsx
+++ src/pages/AddJobs/AddJob.jsx
@@ -6,15 +6,130 @@ const AddJob = () => {
       <h2>Please add a job</h2>
       <form>
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-          <legend className="fieldset-legend">Login</legend>
+          <legend className="fieldset-legend">Basic Info</legend>
 
-          <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <label className="label">Job Title</label>
+          <input
+            name="title"
+            type="text"
+            className="input"
+            placeholder="Job Title"
+          />
 
-          <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <label className="label">Company</label>
+          <input
+            name="company"
+            type="text"
+            className="input"
+            placeholder="Company Name"
+          />
 
-          <button className="btn btn-neutral mt-4">Login</button>
+          <label className="label">Location</label>
+          <input
+            name="location"
+            type="text"
+            className="input"
+            placeholder="Company Location"
+          />
+          <label className="label">Company Logo</label>
+          <input
+            name="company_logo"
+            type="text"
+            className="input"
+            placeholder="Company Logo URL"
+          />
+        </fieldset>
+        {/* Job Type */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Basic Info</legend>
+
+          <div className="filter">
+            <input
+              className="btn filter-reset"
+              type="radio"
+              name="jobType"
+              aria-label="All"
+            />
+            <input
+              className="btn"
+              type="radio"
+              name="jobType"
+              aria-label="On-Site"
+            />
+            <input
+              className="btn"
+              type="radio"
+              name="jobType"
+              aria-label="Remote"
+            />
+            <input
+              className="btn"
+              type="radio"
+              name="jobType"
+              aria-label="Hybrid"
+            />
+          </div>
+        </fieldset>
+        {/* Job Category */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Basic Info</legend>
+
+          <select
+            defaultValue="Pick a color"
+            name="category"
+            className="select"
+          >
+            <option disabled={true}>Job Category</option>
+            <option>Engineering</option>
+            <option>Sales</option>
+            <option>Marketing</option>
+            <option>Finance</option>
+          </select>
+        </fieldset>
+        {/* Application Deadline */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Application Deadline</legend>
+          <input type="date" className="input" />
+        </fieldset>
+        {/* Salary Range */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Salary Range</legend>
+
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+            <div className="">
+              <label className="label">Minimum Salary</label>
+              <input
+                name="salaryMin"
+                type="text"
+                className="input"
+                placeholder="Minimum Salary"
+              />
+            </div>
+            <div className="">
+              <label className="label">Maximum Salary</label>
+              <input
+                name="salaryMax"
+                type="text"
+                className="input"
+                placeholder="Maximum Salary"
+              />
+            </div>
+
+            <div className="">
+              <label className="label">Currency</label>
+              <select
+                defaultValue="Pick a color"
+                name="category"
+                className="select"
+              >
+                <option disabled={true}>Select a Currency</option>
+                <option>BDT</option>
+                <option>USD</option>
+                <option>Euro</option>
+                <option>Pound</option>
+              </select>
+            </div>
+          </div>
         </fieldset>
       </form>
     </div>
