75f3ccc97dcc9fd58f23f8402b326f215fde3fea
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-06-02 20:08:01 +0600

    added table for the recruiter posted jobs

--- src/pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs/MyPostedJobs.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
+import useAuth from "../../hooks/useAuth";
+import JobsLists from "./JobsLists";
+import { jobsCreatedByPromise } from "../../api/jobsApi";
 
 const MyPostedJobs = () => {
-  return <div></div>;
+  const { user } = useAuth();
+  return (
+    <div>
+      My Posted Jobs
+      <Suspense>
+        <JobsLists
+          jobsCreatedByPromise={jobsCreatedByPromise(user.email)}
+        ></JobsLists>
+      </Suspense>
+    </div>
+  );
 };
 
 export default MyPostedJobs;
